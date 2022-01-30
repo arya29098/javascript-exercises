d406a9567125a9d7bbc9cb2e129c46d4b963f609
Arya <aryacittaadijaya@gmail.com>
2022-01-31 05:17:44 +0800

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    let loopMax = myArray.length;
 
+    for(let i=0;i<args.length;i++){
+        for(let j=0;j<loopMax;j++){
+            if (myArray[j]===args[i]){
+                myArray.splice(j,1);
+                loopMax = loopMax - 1;
+                j = j - 1;
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
