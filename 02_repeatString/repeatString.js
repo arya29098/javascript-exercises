aa66649354605ade2c54f96a5c63d8dbb06d187a
Arya <aryacittaadijaya@gmail.com>
2022-01-31 04:41:43 +0800

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase,numRepeat) {
+    if (numRepeat < 0){
+        return "ERROR";
+    }
+    let phraseHolder = "";
+    for (let i = 1; i<=numRepeat; i++){
+        phraseHolder = phraseHolder + phrase;
+    }
+    return phraseHolder;
 };
 
 // Do not edit below this line
